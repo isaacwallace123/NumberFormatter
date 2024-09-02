1b147b171b83e71bfd7b4330451e07b34cda4aaa
isaacwallace123 <74162185+isaacwallace123@users.noreply.github.com>
2024-09-02 02:58:22 -0400

    Create Main.lua
    The main script using Knit package as the framework

---
+++ Main.lua
@@ -0,0 +1,49 @@
+local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
+
+local C = Knit.CreateController {
+	Name = `{script.Name}`,
+}
+
+function C.KnitInit()
+	Rep = game:GetService("ReplicatedStorage")
+end
+
+function C.KnitStart()end
+
+--[[     0 --> 999,999,999 --> 1B    ]]
+
+local Suffixes = {"K", "M", "B", "T", "Qd", "Qn", "Sx", "Sp", "Oc", "No", "Dc", "Uc"}
+
+function FormatDecimals(Num:number, Decimals:number) -- sets the decimals for the text
+	return string.format("%." .. Decimals .. "f",Num)
+end
+
+function C:FormatCommas(Number: number)
+	local Formatted = tostring(Number)
+	local Updated, Count = string.gsub(Formatted, "^(-?%d+)(%d%d%d)", '%1,%2')
+	if Count == 0 then
+		return Updated
+	else
+		return C:FormatCommas(Updated)
+	end
+end
+
+function C:Number(Num:number, Decimals:number, ForceENotation:BoolValue)
+	if Num < 1e9 then return C:FormatCommas( Num == math.floor(Num) and FormatDecimals(Num,0) or FormatDecimals(Num,2)) end
+	
+	local Notation = math.floor(math.log(Num,10))
+	
+	local kNotation = math.floor(math.log(Num,1000))
+	
+	local Front = Num / math.pow(1000,kNotation)
+	
+	Decimals = (math.floor(Front) == Front) and 0 or (Decimals or 2)
+	
+	if (kNotation <= #Suffixes and not ForceENotation) then
+		return string.format("%." .. Decimals .. "f",Num / math.pow(1000,kNotation)) .. ((kNotation > 0 and Suffixes[kNotation]) or "")
+	else
+		return string.format("%." .. Decimals .. "f",Num / math.pow(10,Notation)) .. (kNotation > 0 and "e+" .. Notation or "")
+	end
+end
+
+return C
